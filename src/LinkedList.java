8d99b2a6747bacbeea434001d0c48df1eafe79fd
konstanZX2 <zxgenerationzx@gmail.com>
2022-05-25 20:00:34 +0200

    It's getting hard to keep up with the crap I've trashed

---
+++ src/LinkedList.java
@@ -0,0 +1,84 @@
+public class LinkedList {
+    Element firstElement;
+
+
+    public LinkedList(){}
+
+    public void insertFirst(Object object){
+
+    }
+    public void insertLast(Object object){
+
+    }
+    public void print(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public void remove(Object object){
+
+    }
+    public Object getFirstObject(){
+
+    }
+    public Object getLastObject(){
+
+    }
+    public Object getObjectAtPosition(int i){
+
+    }
+
+    public class Element{
+
+
+        Element element;
+        
+        public Element(Object newObject){
+
+        }
+
+        public void setNext(Element newNext){
+
+
+        }
+
+        public Element getNext(){
+
+            return element;
+        }
+
+        public Object getObject(){
+            return object;
+        }
+
+        public void setObject(Object newObject){
+
+        }
+        public void delete(){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
