cbaa169f18c434d0db2e2a8f73dd7cffc16561ae
konstanZX2 <zxgenerationzx@gmail.com>
2022-05-27 17:41:42 +0200

    I just evaluated random code in my console

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,84 +1,86 @@
+import java.util.Scanner;
+
 public class LinkedList {
     Element firstElement;
 
 
-    public LinkedList(){}
-
-    public void insertFirst(Object object){
-
+    public LinkedList() {
+        LinkedList linkedList = new LinkedList();
     }
-    public void insertLast(Object object){
 
-    }
-    public void print(){
+    public void insertFirst(Object object) {
 
-    }
-    public boolean isEmpty(){
 
-    }
-    public void remove(Object object){
-
-    }
-    public Object getFirstObject(){
-
-    }
-    public Object getLastObject(){
-
-    }
-    public Object getObjectAtPosition(int i){
 
     }
 
-    public class Element{
+    public void insertLast(Object object) {
 
+    }
 
-        Element element;
-        
-        public Element(Object newObject){
-
-        }
-
-        public void setNext(Element newNext){
+    public void print() {
+        for (Element firstElement:NextElement){
 
 
         }
 
-        public Element getNext(){
-
-            return element;
-        }
-
-        public Object getObject(){
-            return object;
-        }
-
-        public void setObject(Object newObject){
-
-        }
-        public void delete(){
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-}
+
+    public boolean isEmpty() {
+        return firstElement == null;
+    }
+        public void remove (Object object){
+
+        }
+
+        public Object getFirstObject () {
+        return firstElement;
+        }
+
+        public Object getLastObject () {
+
+        }
+
+        public Object getObjectAtPosition ( int i){
+
+        }
+
+        private class Element {
+
+            Element nextElement;
+            Object object;
+
+
+            public Element(Object newObject) {
+                this.object = newObject;
+
+            }
+
+            public void setNextElement(Element newNextElement) {
+                this.nextElement = newNextElement;
+
+            }
+
+            public Element getNextElement() {
+
+                return nextElement;
+            }
+
+            public Object getObject() {
+                return object;
+            }
+
+            public void setObject(Object newObject) {
+                object = newObject;
+            }
+
+            public void delete() {
+                nextElement = null;
+                object = null;
+
+            }
+
+
+        }
+    }
+
